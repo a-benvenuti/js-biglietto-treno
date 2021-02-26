7f5ef453e139cfa962e8f6e3a3664f921f072a92
Andrea <b.a.benvenuti3@gmail.com>
2021-02-26 15:57:27 +0100

    creato programma per il calcolo del prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-var kmPercorsi = prompt("quanti Km deve percorrere?");
-var etaPasseggero = prompt("Quanti anni ha?");
+var kmPercorsi = Math.abs((prompt("quanti Km deve percorrere?")));
+var etaPasseggero = Math.abs((prompt("Quanti anni ha?")));
 var prezzoIntero = (kmPercorsi * 0.21).toFixed(2);
 var prezzoOver65 = (prezzoIntero - (prezzoIntero * 0.4)).toFixed(2);
 var prezzoUnder18 = (prezzoIntero - (prezzoIntero * 0.2)).toFixed(2);
 
 if (isNaN(kmPercorsi)  || isNaN(etaPasseggero)) {
   alert("ERRORE! Km percorsi o eta' Non sono stati inseriti correttamente.");
-  kmPercorsi = prompt("quanti Km deve percorrere?");
-  etaPasseggero = prompt("Quanti anni ha?");
+  alert("RICARICARE LA PAGINA");
 } else  {
-  console.log(prezzoIntero);
-  console.log(prezzoOver65);
-  console.log(prezzoUnder18);
+  if (etaPasseggero > 0 && etaPasseggero < 18 ) {
+    document.getElementById('stamp').innerHTML = ("il prezzo del suo biglietto è: " + prezzoUnder18 + "€");
+    console.log("prezzo Minorenne");
+  } else if (etaPasseggero >= 65) {
+    document.getElementById('stamp').innerHTML = ("il prezzo del suo biglietto è: " + prezzoOver65 + "€");
+    console.log("prezzo Over65");
+  } else {
+    document.getElementById('stamp').innerHTML = ("il prezzo del suo biglietto è: " + prezzoIntero + "€");
+    console.log("prezzo Intero");
+  }
 }
